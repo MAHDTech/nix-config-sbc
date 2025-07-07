b3249ad74688929b995c35ff79c9359afb28af1f
MAHDTech <MAHDTech@saltlabs.tech>
2025-07-07 11:45:37 +1000

    fix: cleanup

--- modules/uefi-image/rk3588-raw.nix
+++ modules/uefi-image/rk3588-raw.nix
@@ -5,31 +5,12 @@
   ...
 }:
 {
+  imports = [
+    ./base.nix
+  ];
+
   formatAttr = "rk3588-raw-efi";
   fileExtension = ".img";
-
-  boot = {
-    initrd.availableKernelModules = [ "ext4" ];
-    supportedFilesystems = [ "ext4" ];
-    loader = {
-      systemd-boot.enable = lib.mkForce true;
-      efi.canTouchEfiVariables = lib.mkForce true;
-      grub.enable = lib.mkForce false;
-      grub.device = "nodev";
-    };
-  };
-
-  fileSystems = lib.mkForce {
-    "/" = {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-    };
-
-    "/boot" = {
-      device = "/dev/disk/by-label/ESP";
-      fsType = "vfat";
-    };
-  };
 
   system.build.rk3588-raw-efi = lib.mkForce (
     import "${toString pkgs.path}/nixos/lib/make-disk-image.nix" {
